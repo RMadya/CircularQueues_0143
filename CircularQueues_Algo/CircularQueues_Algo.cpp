8ccd0dae24827ba5a4f206cdce15e4ebc6538756
RMadya <kereyoben75@gmail.com>
2024-05-31 11:20:37 +0700

    while front position dan rear

--- CircularQueues_Algo/CircularQueues_Algo.cpp
+++ CircularQueues_Algo/CircularQueues_Algo.cpp
@@ -65,4 +65,12 @@ public:
 		}
 		cout << "\nElement in the queue are...\n";
 
+		if (FRONT_position == REAR_position) {
+			while (FRONT_position == REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position;
+			}
+			cout << endl;
+		}
+
 		
