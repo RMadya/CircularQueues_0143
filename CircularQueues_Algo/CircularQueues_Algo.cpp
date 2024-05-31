8b550067b5670951f140133f5bc38eb0c0d74314
RMadya <kereyoben75@gmail.com>
2024-05-31 11:18:17 +0700

    dimulai dari tidak ada antrian / -1

--- CircularQueues_Algo/CircularQueues_Algo.cpp
+++ CircularQueues_Algo/CircularQueues_Algo.cpp
@@ -43,4 +43,16 @@ public:
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
-		
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+
+	
