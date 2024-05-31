8c6cd654d16999b64c685207bbdd26dde4d2ebce
RMadya <kereyoben75@gmail.com>
2024-05-31 11:16:33 +0700

    queue array rear nya num

--- CircularQueues_Algo/CircularQueues_Algo.cpp
+++ CircularQueues_Algo/CircularQueues_Algo.cpp
@@ -16,10 +16,24 @@ public:
 		cin >> num;
 		cout << endl;
 
-		//not commit
+		
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "Queue overflow\n";
 			return;
 		}
-
 		
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+
+		else {
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR - 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	
